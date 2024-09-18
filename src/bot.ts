a83394548f77806dfb39636a8783286c86550f37
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-09-17 22:26:56 -0400

    first commit

---
+++ src/bot.ts
@@ -0,0 +1,35 @@
+import { Bot, InlineKeyboard } from "./deps.ts";
+
+const BOT_TOKEN = Deno.env.get("BOT_TOKEN") as string;
+
+export const bot = new Bot(BOT_TOKEN);
+const kv = await Deno.openKv();
+
+bot
+  .command("set")
+  .filter((ctx) => {
+    return ctx.from?.id === Deno.env.get("ADMIN_ID");
+  })
+  .use(async (ctx) => {
+    await kv.set(["link"], ctx.match);
+    await ctx.react("🏆");
+  });
+
+bot.on("channel_post", async (ctx) => {
+  if (ctx.channelPost.forward_origin || ctx.channelPost.media_group_id) {
+    return;
+  }
+
+  const link = await kv.get<string>(["link"]);
+
+  if (!link.value) {
+    return;
+  }
+
+  await ctx.editMessageReplyMarkup({
+    reply_markup: new InlineKeyboard().url(
+      "Entra aqui para apoyar al canal ",
+      link.value
+    ),
+  });
+});
---
+++ src/deps.ts
@@ -0,0 +1,4 @@
+// x/
+export * from "https://deno.land/x/grammy@v1.27.0/mod.ts";
+export * from "https://deno.land/x/grammy_ratelimiter@v1.2.0/mod.ts"; // https://grammy.dev/plugins/ratelimiter
+export * from "https://deno.land/x/grammy_menu@v1.2.2/mod.ts"; // https://grammy.dev/plugins/menu
